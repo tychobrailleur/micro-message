88a606442a3d6dbbe923522447a8ba3bda12a171
Sébastien Le Callonnec <sebastien@weblogism.com>
2013-03-16 14:51:52 +0000

    Add nokogiri to process incoming message.

--- process-client/src/main/ruby/main.rb
+++ process-client/src/main/ruby/main.rb
@@ -1,4 +1,4 @@
-puts 'Starting...'
+require 'nokogiri'
 
 java_import 'org.apache.camel.CamelContext'
 java_import 'org.apache.camel.impl.DefaultCamelContext'
@@ -6,6 +6,21 @@ java_import 'org.apache.camel.impl.DefaultCamelContext'
 context = DefaultCamelContext.new
 endpoint = context.get_endpoint("activemq:test.queue")
 consumer = endpoint.create_polling_consumer()
-exchange = consumer.receive()
 
-puts 'Done.'
+puts 'Starting...'
+while true
+  exchange = consumer.receive()
+  message = exchange.get_in().body
+
+  # Process incoming xml message
+  doc = Nokogiri::XML(message)
+  code = doc.at_xpath("/message/code").text()
+  
+  # Exit when code is END.
+  if code == "END"
+    puts "Bye."
+    Java::JavaLang::System::exit(0)
+  else
+    puts "Code: #{code}"
+  end
+end
