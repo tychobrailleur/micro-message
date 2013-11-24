8d45b02fdf4dabd73af295ea9d86839f1576d977
Sébastien Le Callonnec <sebastien@weblogism.com>
2013-11-24 14:03:04 +0000

    Extract class to fix issue with recent version of JRuby.

--- process-client/src/main/ruby/main.rb
+++ process-client/src/main/ruby/main.rb
@@ -4,6 +4,7 @@ java_import 'org.apache.camel.CamelContext'
 java_import 'org.apache.camel.Exchange'
 java_import 'org.apache.camel.impl.DefaultCamelContext'
 java_import 'org.apache.camel.ProducerTemplate'
+java_import 'org.apache.camel.Processor'
 java_import 'org.apache.camel.impl.DefaultProducerTemplate'
 java_import 'org.apache.camel.builder.RouteBuilder'
 
@@ -11,24 +12,32 @@ context = DefaultCamelContext.new
 endpoint = context.get_endpoint("activemq:test.queue")
 consumer = endpoint.create_polling_consumer()
 
+class ParseMessageProcessor
+  java_implements 'Processor'
+
+  java_signature 'void process(Exchange exchange)'
+  def process(exchange)
+    message = exchange.get_in().body
+
+    # Process incoming xml message
+    doc = Nokogiri::XML(message)
+    code = doc.at_xpath("/message/code").text()
+    
+    # Exit when code is END.
+    if code == "END"
+      puts "Bye."
+      Java::JavaLang::System::exit(0)
+    else
+      exchange.get_out().body = "<code>#{code}</code>"
+    end
+  end
+end
+
 
 class ClientRouter < RouteBuilder
   def configure
-    from("activemq:test.queue").process do |exchange|
-      message = exchange.get_in().body
-
-      # Process incoming xml message
-      doc = Nokogiri::XML(message)
-      code = doc.at_xpath("/message/code").text()
-  
-      # Exit when code is END.
-      if code == "END"
-        puts "Bye."
-        Java::JavaLang::System::exit(0)
-      else
-        exchange.get_out().body = "<code>#{code}</code>"
-      end
-    end.to("direct:path")
+    from("activemq:test.queue").process(ParseMessageProcessor.new)
+      .to("direct:path")
 
     from("direct:path").wire_tap("direct:audit")
       .set_header(Exchange::HTTP_METHOD, constant("POST"))
